00a6f116cdcdf323cf11f0b8abad128740629012
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2023-09-10 18:32:43 +0700

    add: use bun v1

--- flake.nix
+++ flake.nix
@@ -20,6 +20,30 @@
           customNodejs = pkgs.nodejs-_x.override {
             openssl = pkgs.openssl_1_1;
           };
+          bunV1 = pkgs.bun.overrideAttrs (final: prev: with pkgs; prev // rec {
+            version = "1.0.0";
+            src = passthru.sources.${stdenvNoCC.hostPlatform.system} or (throw "Unsupported system: ${stdenvNoCC.hostPlatform.system}");
+            passthru = prev.passthru // {
+              sources =  prev.passthru.sources // {
+                "aarch64-darwin" = fetchurl {
+                  url = "https://github.com/oven-sh/bun/releases/download/bun-v${version}/bun-darwin-aarch64.zip";
+                  sha256 = "sha256-Bd8KL1IbWBRiMZq4YPhNLdhBOqRReCFeUPAilLfk0TM=";
+                };
+                "x86_64-darwin" =  fetchurl {
+                  url = "https://github.com/oven-sh/bun/releases/download/bun-v${version}/bun-darwin-x64.zip";
+                  sha256 = "sha256-0AT58hjawS60q5YAQd/upVz0vOIs11JM+lc3c1mGyOE=";
+                };
+                "aarch64-linux" =  fetchurl {
+                  url = "https://github.com/oven-sh/bun/releases/download/bun-v${version}/bun-linux-aarch64.zip";
+                  sha256 = "sha256-CHOiQ47wXjkFyJG9ElE9gBpmWpylMEUf6c+Sm+YCpGc=";
+                };
+                "x86_64-linux" =  fetchurl {
+                  url = "https://github.com/oven-sh/bun/releases/download/bun-v${version}/bun-linux-x64.zip";
+                  sha256 = "sha256-1ju7ZuW82wRfXEiU24Lx9spCoIhhddJ2p4dTTQmsa7A=";
+                };
+              };
+            };
+          });
         in
         pkgs.devshell.mkShell {
           name = "maulana.id";
@@ -37,6 +61,10 @@
             {
               name = "node";
               package = pkgs.nodejs;
+            }
+            {
+              name = "bun";
+              package = bunV1;
             }
           ];
           packages = [  ];
